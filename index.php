8cdfa93f4464aeb7002f9ab66f0c6e21e724393d
Doni Mustasar <donny.md17@gmail.com>
2019-03-19 20:13:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
  <body>
  <h1>Are you a football fans ? Register here!</h1>
 
- <h1 style="float: right;">Test Using CV Image Analyzing</h1>
+ <a href="cv.php" style="float: right;">Test Using CV Image Analyzing</a>
 
  <p>Fill in your name and your favorite football club, then click <strong>Submit</strong> to register.</p>
  <form method="post" action="index.php" enctype="multipart/form-data" >
